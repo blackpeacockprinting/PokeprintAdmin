fdc6d3260a226830d7b94d2bdaed4d1edde929ee
blackpeacockprinting <trevorfgoss@gmail.com>
2026-04-12 11:52:15 +0900

    Add PWA support (manifest, service worker, icons)

---
+++ sw.js
@@ -0,0 +1,36 @@
+const CACHE = 'bpp-v1';
+const SHELL = ['/PokeprintAdmin/', '/PokeprintAdmin/index.html'];
+const API_ORIGINS = ['us-central1.run.app'];
+
+self.addEventListener('install', e => {
+  e.waitUntil(caches.open(CACHE).then(c => c.addAll(SHELL)));
+  self.skipWaiting();
+});
+
+self.addEventListener('activate', e => {
+  e.waitUntil(caches.keys().then(keys =>
+    Promise.all(keys.filter(k => k !== CACHE).map(k => caches.delete(k)))
+  ));
+  self.clients.claim();
+});
+
+self.addEventListener('fetch', e => {
+  const url = e.request.url;
+  const isAPI = API_ORIGINS.some(o => url.includes(o));
+
+  if (isAPI) {
+    e.respondWith(
+      fetch(e.request)
+        .then(res => {
+          const clone = res.clone();
+          caches.open(CACHE).then(c => c.put(e.request, clone));
+          return res;
+        })
+        .catch(() => caches.match(e.request))
+    );
+  } else {
+    e.respondWith(
+      caches.match(e.request).then(cached => cached || fetch(e.request))
+    );
+  }
+});
